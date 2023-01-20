4e160a5d54755b9cfd0d0d5375724fc2b537974c
ayaquro <121092738+ayaquro@users.noreply.github.com>
2023-01-20 09:25:10 +0000

    [add]

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "homes#top"
+  devise_for :users
+  resources :books, only: [:new, :create, :index, :show]
+  get 'books/edit'
+  get "homes/about" => "homes#about", as: "about"
+
 end
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
---
+++ db/migrate/20230119121812_remove_title_from_book.rb
@@ -0,0 +1,5 @@
+class RemoveTitleFromBook < ActiveRecord::Migration[6.1]
+  def change
+    remove_column :books, :title, :string
+  end
+end
