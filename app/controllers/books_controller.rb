7d8db2a3a2a89598933af5656516d2497045bbd9
ayaquro <121092738+ayaquro@users.noreply.github.com>
2023-01-21 05:17:39 +0000

    [add]image

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,7 +7,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
-    redirect_to books_path
+    redirect_to book_path
   end
 
   def index
@@ -15,10 +15,19 @@ class BooksController < ApplicationController
   end
 
   def show
-    @book = 
+    @book = Book.find(params[:id])
   end
 
   def edit
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+
+  def updated
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  root to: "homes#top"
   devise_for :users
-  resources :books, only: [:new, :create, :index, :show]
+  root to: "homes#top"
+  resources :books
+  resources :users, only: [:show, :edit]
   get 'books/edit'
   get "homes/about" => "homes#about", as: "about"
 
 end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
