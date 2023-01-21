53432edbd63d8f41c5b8d527f62d6f027a26a980
ayaquro <121092738+ayaquro@users.noreply.github.com>
2023-01-21 14:29:11 +0000

    [add]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,14 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @book = @user.books
+    @books = @user.books
   end
 
   def edit
+  end
+   private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
   end
 end
--- db/migrate/20230119121812_remove_title_from_book.rb
+++
@@ -1,5 +0,0 @@
-class RemoveTitleFromBook < ActiveRecord::Migration[6.1]
-  def change
-    remove_column :books, :title, :string
-  end
-end
