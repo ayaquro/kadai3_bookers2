708318ac4bfc3a03e699707ad7a1cec47bf27f3d
ayaquro <121092738+ayaquro@users.noreply.github.com>
2023-01-22 17:03:38 +0000

    [add]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
+
   def show
     @user = User.find(params[:id])
     @books = @user.books
@@ -7,14 +9,28 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
-   private
+
+  def index
+    @books = Book.all
+    @book = Book.new
+    @user = current_user
+  end
+
+  def update
+  end
+
+  private
 
   def user_params
     params.require(:user).permit(:name, :profile_image, :introduction)
   end
 
-  def index
-    
+  def is_matching_login_user
+    user_id = params[:id].to_i
+    unless user_id == current_user.id
+      redirect_to books_path
+    end
   end
+
 
 end
